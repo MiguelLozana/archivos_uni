2d263395c47c08c09fc9286669eb39e32be53b71
MiguelLozana <miguellozanacueto@gmail.com>
2025-10-04 20:52:30 +0200

    2051Sabado4oct25

--- Introducion Programacion/Trabajo en casa/LozanaCuetoMiguel-S4L6/personSession4Task/Person.java
+++ Introducion Programacion/Trabajo en casa/LozanaCuetoMiguel-S4L6/personSession4Task/Person.java
@@ -1,29 +1,37 @@
 
 /**
- * Modela una Persona incluyendo al menos su nombre, apellido, edad y género.
+ * Modela una persona incluyendo al menos su nombre, apellido, edad y género.
  * @author Miguel Lozana
  * @version 20250910
  */
 public class Person {
+    //Constantes
     public final static int MIN_AGE=0;
     public final static int MAX_AGE=120;
     public final static int ADULTHOOD_AGE = 18;
     public final static int RETIREMENT_AGE = 65;
     public final static boolean GENDER_FEMALE = false;
     public final static boolean GENDER_MALE = true;
+    public final static int DEFAULT_INIT_INDEX=0;
+    public final static int MIN_NAME_LENGTH =2;
+    public final static int MIN_SURNAME_LENGTH =4;
     
-    public static final String DEFAULT_NAME = "Fernando" ;
-    public static final String DEFAULT_SURNAME = "Alonso" ;
-    public static final int DEFAULT_AGE = 44 ;
-    public static final boolean DEFAULT_GENDER= GENDER_MALE;
+    // Constantes → Valores predeterminados para los atributos
+
+    public final static String DEFAULT_NAME = "Fernando" ;
+    public final static String DEFAULT_SURNAME = "Alonso" ;
+    public final static int DEFAULT_AGE = 44 ;
+    public final static boolean DEFAULT_GENDER= GENDER_MALE;
     
+    //Atributos
     private String name; 
     private String surname;
     private int age;
     private boolean gender; //Genero masculino=true; false=femenino
     
+    //Contructores (Completo de parametros 4/por defecto/ Solo para la edad / Nombre edad y genero / Nombre y apellido
     /**
-     * Constructor por defecto de la clase Person.
+     * Constructor completo con todos los parametros
      * @param name, nombre de la persona
      * @param surname, apellido de la persona
      * @param age, edad de la persona
@@ -52,7 +60,7 @@ public class Person {
      * Constructor para el nombre, edad y genero de la clase Person.
      * @param name nombre con la que se va a construir el objeto
      * @param age edad con la que se va a construir el objeto
-     * @param gender genero con la que se va a construir el objeto
+     * @param gender género con la que se va a construir el objeto
      */
     public Person(String name, int age, boolean gender){
         this(name,DEFAULT_SURNAME, age ,gender);
@@ -67,10 +75,10 @@ public class Person {
     }
     
     
-    
+    //Setters de los atributos
     /**
-     * Cambia el valor del atributo nombre por uno dado como parámetro.
-     * @param newName Sustituye el antiguo valor del atributo nombre.
+     * Cambia el valor del atributo nombre por uno dado como parámetro, siempre que no sea null.
+     * @param name Sustituye el antiguo valor del atributo nombre.
      */
     private void setName (String name) {
         checkParam(name != null,"Nombre Ilegal");
@@ -78,16 +86,15 @@ public class Person {
     }
     /**
      *
-     *Reemplaza el valor de atributo surname por otro dado como parámetro. 
-     * @param newSuranme Sustituye el antiguo valor del atributo surname
+     * Reemplaza el valor de atributo surname por otro dado como parámetro. 
+     * @param surname Sustituye el antiguo valor del atributo surname
      */
     private void setSurname (String surname){
        checkParam(surname != null,"Apellido Ilegal");
        this.surname=surname;
     }    
     /**
-     *Cambia el valor del atributo edad por uno nuevo dado como parametro siempre que una vez llamado 
-     *checkParam, se cumple la condicion y el valor esté entre MIN_AGE y MAX_AGE    
+     * Cambia el valor del atributo edad por uno nuevo dado como parametro siempre que este en el rango establecido     
      * @param newAge nuevo valor para la edad de la persona
      */
     private void setAge (int age){
@@ -96,14 +103,14 @@ public class Person {
     }
     /**
      * Cambia el valor del atributo gender por otro proporcionado como parámetro.
-     * @param newGender sustituye el valor gender y pasa a ser el nuevo valor del atributo gender 
+     * @param gender nuevo valor para el genero de la persona
      */
     private void setGender (boolean gender){
        this.gender=gender;
     }
     
     
-    
+    //Getters de los atibutos 
     /**
      * Devuelve el valor actual del atributo name.
      * @return String con el nombre almacenado en el atributo name.
@@ -132,10 +139,26 @@ public class Person {
     public boolean getGender(){
         return gender;
     }
+    
+    
+    //Metodos principales
+    
+    /**
+     * Debuelve una cadena con informacion en el siguente formato. Ej: 44-FE-ALVA-ADULT 
+     * @return string con el formato Ej: 44-FE-ALVA-ADULT 
+     */
+    public String getHashCode(){
+        String stringName = shorterString(this.name, DEFAULT_INIT_INDEX, MIN_NAME_LENGTH);
+        String stringSurname = shorterString(this.surname, DEFAULT_INIT_INDEX, MIN_SURNAME_LENGTH);
+        return(this.age+"-"+
+               stringName.toUpperCase()+"-"+
+               stringSurname.toUpperCase()+"-"+
+               this.ageClasifier());
+    }
     /**
      * Devuelve un valor numerico en funcion de la edad actual.
-     * - Si es menor de 18 años: devuelve los años que faltan para la mayoría de edad.
-     * - Si tiene entre 18 y 64 años: devuelve los años que faltan para la jubilación (65 años).
+     * - Si es menor de 18 años: devuelve los años que faltan para ser mayor de edad.
+     * - Si tiene entre 18 y 64 años: devuelve los años que faltan para jubilarse (65 años).
      * - Si tiene 65 o más: devuelve los años que han pasado desde la jubilación.
      *@return int el numero de años segun la edad actual.
      */
@@ -151,49 +174,7 @@ public class Person {
         }
     }
     /**
-     * Debuelve una cadena con informacion en el siguente formato. 
-     * Edad-NOMBRE-longitudDelNombre-apellido-longitudDelApellido
-     * ejemplo:44-FE-ALVA-ADULT 
-     */
-    public String getHashCode(){
-        checkParam(name != null || surname!=null, "Los atributos name o surname son nulos");
-        
-        String stringName = this.name.substring(0,2);
-        String stringSurname = this.surname.substring(0,4);
-        
-        return(age+"-"+
-               stringName.toUpperCase()+"-"+
-               stringSurname.toUpperCase()+"-"+
-               this.ageClasifier());
-    }
-    
-    private String ageClasifier(){
-        if (age <ADULTHOOD_AGE){
-            return "CHILD";
-        } else {
-            if (age<RETIREMENT_AGE){
-                return "ADULT";
-            } else{
-                return "RETIRED";
-            }
-        }
-    }
-    
-    /**
-     * Comprueba si un parámetro tiene sentido dentro del campo correspondiente, en caso contrario corta la ejecucion y muestra un mensaje
-     * @param condition, la condición que el parametro tiene que cumplir
-     * @param message, el mensaje mostrado en el caso de que no se cumpla la condición.
-     */
-    private void checkParam(boolean condition, String message){
-        if(!condition){
-            throw new RuntimeException(message);
-        }
-    }
-    
-    
-    
-    /**
-     * Primero muestar una String con el siguiente formato: Mi edad es X pero el año que viene tendré x+1 años
+     * Primero muestra una String con el siguiente formato: Mi edad es X pero el año que viene tendré x+1 años
      * Luego muestra la representacion de la información devuelta por el metodo toString.
      */
     public void print() {
@@ -201,10 +182,8 @@ public class Person {
         System.out.println(toString());
     }
 
-    
-    
     /**
-     * Crea una representación de la información del objeto con el siguiente formato: nombre-apellido-edad-masculino (o femenino)
+     * Crea una representación de la información del objeto con el siguiente formato: NOMBRE-apellido-edad-masculino (o femenino)
      * @return Devuelve la String con el formato anterior 
      */
     public String toString(){
@@ -216,5 +195,49 @@ public class Person {
         }
         return getName().toUpperCase()+"-"+getSurname()+"-"+getAge()+"-"+sGender;
 
+    }
+    
+    
+    
+    //Metodos auxiliares
+    /**
+     * Si la cadena es suficentemente larga y el indice final es mayor que el inical, devuelve una subcadena en funcion a los parametros
+     * @param cadena Cadena a obtener la subcadena
+     * @param initIndex Valor inicial para obtener la subcadena
+     * @param finalIndex Valor final para obtener la subcadena
+     * @return String Subcadena en fuicnion a los parametros
+     */
+    public String shorterString(String cadena, int initIndex, int finalIndex){
+        checkParam(initIndex<finalIndex, "El indice inicial (segundo parametro) ha de ser menor al indice final(tercer parametro)");
+        int length = finalIndex-initIndex;
+        checkParam(cadena.length()>=length, "La longitud de la cadena es demasiado corta");
+        return cadena.substring(initIndex, finalIndex);     
+    }
+    /**
+     * Clasifica en funcion al gupo de edad. 
+     * - Si es menor de 18 años devuelve "CHILD"
+     * - Si tiene entre 18 y 64 años devuelve "ADULT"
+     * - Si tiene 65 años o más devuelve "RETIRED"
+     */
+    private String ageClasifier(){
+        if (age <ADULTHOOD_AGE){
+            return "CHILD";
+        } else {
+            if (age<RETIREMENT_AGE){
+                return "ADULT";
+            } else{
+                return "RETIRED";
+            }
+        }
+    }
+    /**
+     * Comprueba si un parámetro tiene sentido dentro del campo correspondiente, en caso contrario corta la ejecucion y muestra un mensaje
+     * @param condition, la condición que el parametro tiene que cumplir
+     * @param message, el mensaje mostrado en el caso de que no se cumpla la condición.
+     */
+    private void checkParam(boolean condition, String message){
+        if(!condition){
+            throw new RuntimeException(message);
+        }
     }
 }
--- Introducion Programacion/Trabajo en casa/LozanaCuetoMiguel-S4L6/personSession4Task/PersonTest.java
+++ Introducion Programacion/Trabajo en casa/LozanaCuetoMiguel-S4L6/personSession4Task/PersonTest.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Escenarios de prueba del getAge()
+ * Escenarios de prueba del setAge()
  * 1 - Edad entre los límites - se modifica
  * 2 - Edad por debajo del limite inferior - excepcion
  * 3 - Edad por encima del limite superior - excepcion
  * 4 - Edad en limite inferior - excepcion
  * 5 - Edad en limite superior - excepcion
  *
- * 
- * 
  * @author  Miguel Lozana 
  * @version (26/09/2025)
  *
@@ -23,10 +21,10 @@ import org.junit.jupiter.api.Test;
 public class PersonTest
 {
     /**
-     * Comprueba que en el getAge(), si la edad esta entre los limites se modifica.
+     * 1 - Comprueba que en el setAge(), si la edad esta entre los limites se modifica.
      */
     @Test
-    public void setBetweenLimitAgeTest(){
+    public void setAgeBetweenLimitAgeTest(){
         Person p1 = new Person(8);
         assertEquals(8,p1.getAge());
         
@@ -34,11 +32,11 @@ public class PersonTest
     }
     
     /**
-     * Comprueba que en el getAge(), si la edad esta por debajo del límite inferior se lanza una excepción. 
+     * 2 - Comprueba que en el setAge(), si la edad esta por debajo del límite inferior se lanza una excepción. 
      * Para ello intenta asignarle un valor "incorrecto" al objeto, si lo consigue el test falla.
      */
     @Test
-    public void setUnderLimitAgeTest(){
+    public void setAgeUnderLimitAgeTest(){
         try {
             Person p1 = new Person(-5);
             fail("Se esperaba excepción");
@@ -48,11 +46,11 @@ public class PersonTest
         }
     }
     /**
-     * Comprueba que en el getAge(), si la edad esta por encima del límite superior se lanza una excepción. 
+     * 3 - Comprueba que en el setAge(), si la edad esta por encima del límite superior se lanza una excepción. 
      * Para ello intenta asignarle un valor "incorrecto" al objeto, si lo consigue el test falla.
      */
     @Test
-    public void setAboveLimitAgeTest(){
+    public void setAgeAboveLimitAgeTest(){
         try {
             Person p1 = new Person(333);
             fail("Se esperaba excepción");
@@ -62,11 +60,11 @@ public class PersonTest
         }
     }
     /**
-     * Comprueba que en el getAge(), si la edad está justo en el límite inferior se lanza una excepción. 
+     * 4 - Comprueba que en el setAge(), si la edad está justo en el límite inferior se lanza una excepción. 
      * Para ello intenta asignarle un valor "incorrecto" al objeto, si lo consigue el test falla.
      */
     @Test
-    public void setOnLowerLimitAgeTest(){
+    public void setAgeOnLowerLimitAgeTest(){
         try {
             Person p1 = new Person(Person.MIN_AGE);
             fail("Se esperaba excepción");
@@ -76,10 +74,10 @@ public class PersonTest
         }
     }
     /**
-     * Comprueba que en el getAge(), si la edad está justo en el límite superior se lanza una excepción. Para ello intenta asignarle un valor "incorrecto" al objeto, si lo cosigue el test falla
+     * 5 - Comprueba que en el setAge(), si la edad está justo en el límite superior se lanza una excepción. Para ello intenta asignarle un valor "incorrecto" al objeto, si lo cosigue el test falla
      */
     @Test
-    public void setUpperLimitAgeTest(){
+    public void setAgeUpperLimitAgeTest(){
         try {
             Person p1 = new Person(Person.MAX_AGE);
             fail("Se esperaba excepción");
@@ -88,125 +86,175 @@ public class PersonTest
             assertEquals("Edad Ilegal", excepcion.getMessage());
         }  
     }
-    
-    /**-- Test getHashCode --
-     * 6 - Valores normales - Cumple perfectamente
-     * 7 - Valores con nombre null -> runtimeException en name
-     * 7 - Valores con apellido null -> runtimeException en apellido
-     * 8 - Valores con nombre y apellido null -> runtimeException en name
-     * 8 - Valores con logitud 1 -> 
-     *
-     */
-    
-    /**
-     * Comprueba que representa bien la cadena en un caso favorable
-     */
-    @Test
-    public void normalValue(){
-        Person p1 = new Person("Pedro","Picapiedra",25,true);
-        assertEquals("25-PE-PICA-ADULT", p1.getHashCode());
-    }
-    /**
-     * Comprueba que representa bien la cadena en un caso donde name es null
-     */
-    @Test
-    public void nameIsNullValue(){
-        
-        try {
-            Person p1 = new Person(null,"picapiedra",25,true);
-            fail("Se esperaba excepción");
-        }   
-        catch (RuntimeException excepcion) {
-            assertEquals("Nombre Ilegal", excepcion.getMessage());
-        } 
-    }
-    /**
-     * Comprueba que representa bien la cadena en un caso donde surname es null
-     */
-    @Test
-    public void surnameIsNullValue(){
-        
-        try {
-            Person p1 = new Person("picapiedra",null,25,true);
-            fail("Se esperaba excepción");
-        }   
-        catch (RuntimeException excepcion) {
-            assertEquals("Apellido Ilegal", excepcion.getMessage());
-        } 
-    }
-    /**
-     * Comprueba que representa bien la cadena en un caso donde name y surname es null
-     */
-    @Test
-    public void nameAndSurnameIsNullValue(){
-        
-        try {
-            Person p1 = new Person(null,null,25,true);
-            fail("Se esperaba excepción");
-        }   
-        catch (RuntimeException excepcion) {
-            assertEquals("Nombre Ilegal", excepcion.getMessage());
-        } 
-    }
-    /**
-     * Comprueba que representa bien la cadena en un caso donde name y surname es null
-     */
-    @Test
-    public void lengthIsLowerThanExpected(){
-        try{
-            Person p1 = new Person("A","AB",25,true);
-        }catch (runtimeException re) {
-            assertEquals(p1.getHashCode(), re.getException());
-        
-        }
-    }
-    
     /**--Test getCriticalAge  --
-     * 10 - Edad menor de 18 -> 18 - Edad
-     * 11 - Edad igual a 18 -> 65-18 =47
-     * 12 - Edad mayor de 18 menor de 65 -> 65 - edad
-     * 13 - Edad igual a 65 ->0
-     * 14 - Edad mayor de 65 -> edad - 65
+     * 6 - Edad menor de 18 -> 18 - Edad
+     * 7 - Edad igual a 18 -> 65-18 =47
+     * 8 - Edad mayor de 18 menor de 65 -> 65 - edad
+     * 9 - Edad igual a 65 ->0
+     * 10 - Edad mayor de 65 -> edad - 65
      */
     
     /**
-     * Comprueba que si la edad es menor de 18, se cumple: 18-Edad
+     * 6 - Comprueba que si la edad es menor de 18, se cumple: 18-Edad
      */
     @Test
-    public void underAdulthood() {
+    public void getCriticalAgeUnderAdulthood() {
         Person p1 = new Person(10);
         assertEquals(8, p1.getCriticalAge()); 
     }
     /**
-     * Comprueba que si la edad es igual a 18, se cumple: 65-18 = 47
+     * 7 - Comprueba que si la edad es igual a 18, se cumple: 65-18 = 47
      */
     @Test
-    public void atAdulthood() {
+    public void getCriticalAgeAtAdulthood() {
         Person p1 = new Person(18);
         assertEquals(47, p1.getCriticalAge());
     }
     /**
-     * Comprueba que si la edad es mayor que 18 pero menor de 65, se cumple: 65-edad 
+     * 8 - Comprueba que si la edad es mayor que 18 pero menor de 65, se cumple: 65-edad 
      */
     @Test
-    public void adultRange() {
+    public void getCriticalAgeAdultRange() {
         Person p1 = new Person(40);
         assertEquals(25, p1.getCriticalAge()); 
     }
     /**
-     * Comprueba que si la edad es igual a 65, se cumple: 65-65 = 0
+     * 9 - Comprueba que si la edad es igual a 65, se cumple: 65-65 = 0
      */
     @Test
-    public void atRetirement() {
+    public void getCriticalAgeAtRetirement() {
         Person p1 = new Person(65);
         assertEquals(0, p1.getCriticalAge());
     }
     @Test
     /**
-     * Comprueba que si la edad es mayor que 65, se cumple: Edad - 65 
+     * 10 - Comprueba que si la edad es mayor que 65, se cumple: Edad - 65 
      */
-    public void overRetirement() {
+    public void getCriticalAgeOverRetirement() {
         Person p1= new Person(80);
         assertEquals(15, p1.getCriticalAge()); 
     }
+    
+    /**Test de ageClasifier() mediante el getHashCode() ya que ageClasifier() es privado
+     * 11 - Edad menor de 18 - CHILD
+     * 12 - Edad igual a 18 - ADULT
+     * 13 - Edad entre 18 y 65  - ADULT
+     * 14 - Edad igual a 65 - RETIRED
+     * 15 - Edad mayor de 65 - RETIRED
+     */
+    /**
+     * 11 - Comprueba que getHashCode() contiene "CHILD" proveniente de ageClasifier() si la edad es menor de 18
+     */
+    @Test
+    public void ageClasiferYoungerThan18(){
+        Person p1 = new Person(Person.ADULTHOOD_AGE-1);
+        assertEquals("17-FE-ALON-CHILD", p1.getHashCode()); 
+    }
+    /**
+     * 12 -Comprueba que getHashCode() contiene "ADULT" proveniente de ageClasifier() si la edad es 18
+     */
+    @Test
+    public void ageClasiferIs18(){
+        Person p1 = new Person(Person.ADULTHOOD_AGE);
+        assertEquals("18-FE-ALON-ADULT", p1.getHashCode()); 
+    }
+    /**
+     * 13 - Compureba getHashCode() contiene "ADULT" proveniente de ageClasifier() si la edad está entre 18 y 65
+     */
+    @Test
+    public void ageClasiferBetween18And65(){
+        Person p1 = new Person(Person.ADULTHOOD_AGE+1);
+        assertEquals("19-FE-ALON-ADULT", p1.getHashCode()); 
+    }
+    /**
+     * 14 - Compureba que getHashCode() contiene "RETIRED" proveniente de ageClasifier() si la edad es 65
+     */
+    @Test
+    public void ageClasiferIs65(){
+        Person p1 = new Person(Person.RETIREMENT_AGE);
+        assertEquals("65-FE-ALON-RETIRED", p1.getHashCode()); 
+    }
+    /**
+     * 15 - Compureba que getHashCode() contiene "RETIRED" proveniente de ageClasifier() si la edad es mayor de 65
+     */
+    @Test
+    public void ageClasiferOlderThan65(){
+        Person p1 = new Person(Person.RETIREMENT_AGE+1);
+        assertEquals("66-FE-ALON-RETIRED", p1.getHashCode()); 
+    }
+    
+    /**
+     * Test de shorterString()
+     * 16 - Casos favorables
+     * 17 - Indices invertidos (indice final < indice inicial) excepcion
+     * 19 - Cadena demasiado pequeña - excepcion
+     * 
+     */
+    /**
+     * 16 - Test de shorter string en casos favorables
+     */
+    @Test
+    public void shortStringFavourableString(){
+    Person p1 = new Person();
+    assertEquals("na", p1.shorterString("Fernando", 3, 5)); 
+    }
+    /**
+     * 17 - Test de shorter string con indices desordenados
+     */
+    @Test
+    public void shortStringUnshortedIndex(){
+        try{   
+            Person p1 = new Person();
+            assertEquals("na", p1.shorterString("Fernando", 2,0)); 
+    
+        } catch (RuntimeException re){
+            assertEquals("El indice inicial (segundo parametro) ha de ser menor al indice final(tercer parametro)", re.getMessage());
+        }
+    }
+    /**
+     * 18 - Test de shorter string con cadena demasiado corta
+     */
+    @Test
+    public void shortStringTooShortString(){
+        try{   
+            Person p1 = new Person();
+            assertEquals("na", p1.shorterString("Fernando",0, 20)); 
+    
+        } catch (RuntimeException re){
+            assertEquals("La longitud de la cadena es demasiado corta", re.getMessage());
+        }
+    }
+    /**-- Test getHashCode --
+     * 19 - Valores con mayusculas y minusculas
+     * 20 - Valores con acentos
+     * 21 - Simbolos como string
+     */
+    
+    /**
+     * 19 - Comprueba que el metodo getHashCode representa bien la cadena con mayusculas y minusculas
+     */
+    @Test
+    public void getHashCodeCapitalAndLowerCase(){
+        Person p1 = new Person("PedRo","PIcApIEdRA",25,true);
+        assertEquals("25-PE-PICA-ADULT", p1.getHashCode());
+    }
+    /**
+     * 20 - Comprueba que representa bien la cadena con acentos
+     */
+    @Test
+    public void getHashCodeAcents(){
+        Person p1 = new Person("Pëdrò","Pîçápiedra",5,true);
+        assertEquals("5-PË-PÎÇÁ-CHILD", p1.getHashCode());
+    }
+    /**
+     * 21 - Comprueba que el metodo getHashCode representa bien la cadena con simbolos como string
+     */
+    @Test
+    public void getHashCodeSimbols(){
+        Person p1 = new Person("[*¨{","^´_-",95,true);
+        assertEquals("95-[*-^´_--RETIRED", p1.getHashCode());
+    }
+   
+    
+    
 }
